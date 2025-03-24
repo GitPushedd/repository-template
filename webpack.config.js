22a81161703e07babe50f9646ccaf7597d2774a7
Maiker Hernández <maikeralejandro@hotmail.com>
2025-03-24 12:29:09 -0500

    Versión básica de plantilla

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { watch, watchFile } = require("fs");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/template.html",
+      favicon: "./src/imgs/sushiLogo.png",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
